8f821a0b79e684606819e2334575161b745b672d
Arvind Tiwari <tiwariav@gmail.com>
2021-06-21 14:29:47 +0530

    fix: topnav banner style

--- components/structures/Topnav/Topnav.js
+++ components/structures/Topnav/Topnav.js
@@ -33,9 +33,7 @@ export default function Topnav({
       {...props}
     >
       {banner ? (
-        <Container className={clsx(styles.container, styles.banner)}>
-          {banner}
-        </Container>
+        <div className={clsx(styles.container, styles.banner)}>{banner}</div>
       ) : null}
       <Container className={styles.container}>
         {logo ? (
