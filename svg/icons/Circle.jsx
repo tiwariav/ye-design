54544b4f0180b0bddfbf03744e1fecafb2cbbe82
Arvind Tiwari <tiwariav@gmail.com>
2021-06-26 02:33:25 +0530

    feat: add svg icon circle

---
+++ svg/icons/Circle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SVGCircle(props) {
+  return (
+    <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <circle cx="5" cy="5" r="5" />
+    </svg>
+  );
+}
