9442371fe06ea93ee23030bd6c34bba5d25a202b
Arvind Tiwari <tiwariav@gmail.com>
2021-11-29 00:47:38 +0530

    chore: update themeprovider

--- components/utilities/ThemeProvider.js
+++ components/utilities/ThemeProvider.js
@@ -1,7 +1,11 @@
 import clsx from "clsx";
 import React from "react";
+import defaultThemeStyleOptions from "../../themes";
 import "../../themes/base.css";
 
-export default function ThemeProvider({ children, theme }) {
-  return <div className={clsx({ theme: theme })}>{children}</div>;
+export default function ThemeProvider({ children, theme, themeStyleOptions }) {
+  const styles = themeStyleOptions
+    ? themeStyleOptions[theme]
+    : defaultThemeStyleOptions[theme];
+  return <div className={clsx({ [styles.root]: theme })}>{children}</div>;
 }
