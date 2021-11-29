a3496fafae85e40da31c42dc87acc55f59bb3852
Arvind Tiwari <tiwariav@gmail.com>
2021-11-29 10:30:45 +0530

    refactor: remove themeprovider

--- themes/index.js
+++ themes/index.js
@@ -1,10 +1,9 @@
-/* eslint css-modules/no-unused-class:off */
 import coolNFreshStyle from "./cool-n-fresh.module.css";
 import trustyStyle from "./trusty.module.css";
 
 const themes = {
-  "cool-n-fresh": coolNFreshStyle,
-  trusty: trustyStyle,
+  "cool-n-fresh": coolNFreshStyle.root,
+  trusty: trustyStyle.root,
 };
 
 export default themes;
