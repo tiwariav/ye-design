7a3e0300faa295b1e4246e2114a009bdf1d7e376
Arvind Tiwari <tiwariav@gmail.com>
2022-08-03 17:07:22 +0530

    chore: fix card stories export

--- components/atoms/sections/index.js
+++ components/atoms/sections/index.js
@@ -1,8 +1,6 @@
 export { default as Bookmark } from "./Bookmark/Bookmark";
-export * as CardStories from "./Card/Card.stories";
+export { default as Card } from "./Card/Card";
 export { default as Container } from "./Container/Container";
 export { default as Divider } from "./Divider/Divider";
 export { default as Hero } from "./Hero/Hero";
-export { Card };
-import Card from "./Card/Card";
-Card.Stories = CardStories;
+export * from "./stories";
---
+++ components/atoms/sections/stories.js
@@ -0,0 +1,3 @@
+export { default as CardMeta } from "./Card/Card.stories";
+export { CardStories };
+import * as CardStories from "./Card/Card.stories";
