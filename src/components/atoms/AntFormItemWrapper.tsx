c1110246157b7f881016dab264b778f2b67a1dde
Arvind Tiwari <tiwariav@gmail.com>
2022-11-23 23:45:13 +0530

    fix: update antd to v5

--- src/components/atoms/AntFormItemWrapper.tsx
+++ src/components/atoms/AntFormItemWrapper.tsx
@@ -1,10 +1,9 @@
-import * as antd from "antd";
+import { Form } from "antd";
 import { clsx } from "clsx";
 import { isDate, isObject } from "lodash-es";
 import React, { ReactElement, useCallback, useMemo, useState } from "react";
 import styles from "./antFormItemWrapper.module.css";
 
-const { Form } = antd;
 interface AntFormItemWrapperProps {
   children: ReactElement;
   className: string;
