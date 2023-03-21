2caf8c6f8ba40a93e09fe21905428844273c54ac
Arvind Tiwari <tiwariav@gmail.com>
2023-03-21 11:52:55 +0530

    fix: fix error on number formatting

--- src/components/atoms/NumberInput/NumberInput.tsx
+++ src/components/atoms/NumberInput/NumberInput.tsx
@@ -1,4 +1,4 @@
-import { isNil, isObject, uniqueId } from "lodash-es";
+import { isNil, isObject, isString, uniqueId } from "lodash-es";
 import {
   forwardRef,
   LegacyRef,
@@ -25,7 +25,7 @@ const NumberInput = forwardRef(
     const formatValue = useCallback(
       (value) => {
         let newFormattedNumber = value;
-        if (!(value && (value.endsWith(".") || value === "-"))) {
+        if (!(isString(value) && (value.endsWith(".") || value === "-"))) {
           const nullValue = value ? "0" : "";
           const formatOptions = isObject(format) ? format : {};
           newFormattedNumber = formatNumber(value, {
