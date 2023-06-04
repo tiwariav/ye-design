846cbcb08b16442631dc564df5a62032d47afa44
Arvind Tiwari <tiwariav@gmail.com>
2023-06-04 07:29:51 +0530

    fix: tsconfig for declaration files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
-import { Config } from "jest";
-// @ts-ignore: TS2835 because jest.config.ts is not read as module by jest
 import { config as sharedConfig } from "wo-library/tools/cjs/jest/config.cjs";
 
-const config: Config = {
-  ...(sharedConfig as Config),
+const config = {
+  ...sharedConfig,
   setupFilesAfterEnv: ["wo-library/tools/cjs/jest/setupTests.cjs"],
   testRegex: "src.*(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
   transformIgnorePatterns: ["/node_modules/(?!(lodash-es|default-import)/)"],
