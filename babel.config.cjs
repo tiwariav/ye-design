2d4a11ec935f95ebb8f1fd40fceb7a72c2a7cb64
Arvind Tiwari <tiwariav@gmail.com>
2023-08-10 16:05:28 +0530

    chore: update babel config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,14 +1,13 @@
-// not used
-
+// used by storybook to add typescript support
 module.exports = {
   plugins: [
     "@babel/plugin-proposal-class-properties",
     [
       "@simbathesailor/babel-plugin-use-what-changed",
       {
-        "active": process.env.NODE_ENV === "development" // boolean
-      }
-    ]
+        active: process.env.NODE_ENV === "development",
+      },
+    ],
   ],
   presets: [
     "@babel/preset-env",
