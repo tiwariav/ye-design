c6a3b775e4342b6a03a4274ea803b563007d6660
Arvind Tiwari <tiwariav@gmail.com>
2023-10-13 01:40:33 +0530

    refactor: add reference to wo-library window types

--- types/wo-library.d.ts
+++ types/wo-library.d.ts
@@ -1,2 +1,3 @@
-/// <reference types="wo-library/types/react.d.ts" />
 /// <reference types="wo-library/types/css.d.ts" />
+/// <reference types="wo-library/types/react.d.ts" />
+/// <reference types="wo-library/types/window.d.ts" />
