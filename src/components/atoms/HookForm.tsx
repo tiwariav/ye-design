a6352c700982fb1dab2a5ae5ff778c162da44fd3
aashishsingla <aashishsingla567@gmail.com>
2023-11-30 20:03:10 +0530

    feat: errors prop in hook form

--- src/components/atoms/HookForm.tsx
+++ src/components/atoms/HookForm.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import {
   FieldValues,
   FormProvider,
+  Path,
   Resolver,
   SubmitErrorHandler,
   SubmitHandler,
@@ -12,6 +14,7 @@ export interface HookFormProps<TFieldValues extends FieldValues>
   extends UseFormProps<TFieldValues> {
   children: React.ReactNode;
   className?: string;
+  errors?: Record<"root" | `root.${string}` | Path<TFieldValues>, string>;
   onInvalid?: SubmitErrorHandler<TFieldValues>;
   onValid?: SubmitHandler<TFieldValues>;
 }
@@ -23,12 +26,25 @@ export default function HookForm<TFieldValues extends FieldValues>({
   children,
   className,
   criteriaMode = "all",
+  errors,
   mode = "onBlur",
   onInvalid,
   onValid,
   ...props
 }: HookFormProps<TFieldValues>) {
   const methods = useForm({ criteriaMode, mode, ...props });
+
+  useEffect(() => {
+    if (errors) {
+      for (const [key, value] of Object.entries(errors)) {
+        methods.setError(key as Path<TFieldValues>, {
+          message: value,
+          type: "manual",
+        });
+      }
+    }
+  }, [errors, methods]);
+
   return (
     <FormProvider {...methods}>
       <form
