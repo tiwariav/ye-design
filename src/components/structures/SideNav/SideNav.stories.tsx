0cd45e69287dcaf450db990589490727c330e339
Arvind Tiwari <tiwariav@gmail.com>
2023-12-07 02:28:41 +0530

    fix: fix SideNav toggled styles on small devices

--- src/components/structures/SideNav/SideNav.stories.tsx
+++ src/components/structures/SideNav/SideNav.stories.tsx
@@ -1,33 +1,60 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { IconMenu } from "@tabler/icons-react";
 import { loremIpsum } from "lorem-ipsum";
 import { ComponentProps } from "react";
 import { randomGradientGenerator } from "wo-library/tools/colors.js";
 
+import LayoutContext from "../../../contexts/LayoutContext/index.js";
 import { BasicSideNav } from "../../__stories/SideNavTemplates.js";
+import Button from "../../atoms/Button/Button.js";
 import SideNav, { SideNavToggle } from "./SideNav.js";
 import { SideNavTitle } from "./SideNavGroup.js";
 
+const SideNavButton = ({ ...props }) => {
+  const {
+    sideNav: { isToggled },
+  } = LayoutContext.useContextState();
+  const {
+    dispatch: { updateSideNav },
+  } = LayoutContext.useContextDispatch();
+
+  return (
+    <>
+      <Button
+        variant="borderless"
+        {...props}
+        onClick={() => updateSideNav({ isToggled: !isToggled })}
+      >
+        <IconMenu />
+      </Button>
+    </>
+  );
+};
+
 const Template = (args: ComponentProps<typeof BasicSideNav>) => (
-  <div
-    className="story-bg-container story-flex"
-    style={{ backgroundImage: randomGradientGenerator(20) }}
-  >
-    <div className="story-flex">
-      <BasicSideNav {...args} />
+  <LayoutContext.LayoutProvider>
+    <div
+      className="story-bg-container story-flex"
+      style={{ backgroundImage: randomGradientGenerator(20) }}
+    >
+      <div className="story-flex">
+        <BasicSideNav {...args} />
+      </div>
+      <div>
+        <SideNavButton />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: loremIpsum({
+              count: 10,
+              format: "html",
+              units: "paragraphs",
+            }),
+          }}
+          style={{ maxWidth: 400 }}
+        />
+      </div>
     </div>
-    <div>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: loremIpsum({
-            count: 10,
-            format: "html",
-            units: "paragraphs",
-          }),
-        }}
-        style={{ maxWidth: 400 }}
-      />
-    </div>
-  </div>
+  </LayoutContext.LayoutProvider>
 );
 
 const metadata: Meta<typeof SideNav> = {
