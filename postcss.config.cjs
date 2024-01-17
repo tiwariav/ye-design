c018c0930f9f1bd47972f37621e0e845767da305
Arvind Tiwari <tiwariav@gmail.com>
2024-01-18 05:14:01 +0530

    fix: preserve media queries

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,4 +3,5 @@ const { getConfig } = require("@tiwariav/postcss-config");
 
 module.exports = getConfig(process.env.NODE_ENV || "development", {
   globalDataOptions: { files: ["./src/styles/media.css"] },
+  presetEnvOptions: { preserveMediaQueries: true },
 });
