5035569dab1a4b4c622a05d90cd892d7b16e0799
Arvind Tiwari <tiwariav@gmail.com>
2024-04-16 20:06:32 +0530

    fix: rename css variables causing build error in rollup

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
   extends: "@tiwariav/stylelint-config",
   rules: {
-    "csstools/value-no-unknown-custom-properties": [
-      true,
-      {
-        importFrom: ["src/styles/base.css"],
-      },
-    ],
     "csstools/media-use-custom-media": [
       "always-known",
       {
         importFrom: ["src/styles/media.css"],
+      },
+    ],
+    "csstools/value-no-unknown-custom-properties": [
+      true,
+      {
+        importFrom: ["src/styles/base.css"],
       },
     ],
   },
